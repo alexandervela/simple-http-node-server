bbc3310b621fd5335e87dbcce78458689a826c94
Alexander Vela <alexander@Alexanders-MacBook-Pro.local>
2018-07-11 23:51:38 -0400

    checkpoint 3 assignment completed

---
+++ server.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+var http = require('http');
+const port = 3000;
+
+const requestHandler = (request, response) => {
+  response.end(`Handling a request on port ${port}`)
+  try {
+    fs.appendFileSync('hello-world.txt', 'Hello to this great world');
+    console.log('Success');
+  } catch (err) {
+    console.log(JSON.stringify(err));
+  }
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log(`You have an error:  ${err}`);
+  }
+
+  console.log(`server is listening on ${port}`);
+});
